09c7f20c1f4c90c5285d6668e92b4b52a6bcd4c2
BHW <kassadin34@gmail.com>
2024-05-08 12:34:18 +0900

    문제 생성 api gcp cloudrun과 연결 req.txt 수정됨 pip install -r req.txt 다시 할것.

--- run/question/views.py
+++ run/question/views.py
@@ -1,4 +1,5 @@
-from constants import GET, SWAGGER_QUESTION_TAG
+import requests
+from constants import GET, MAKE_QUESTION_URL, POST, SWAGGER_QUESTION_TAG
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework import viewsets
 from rest_framework.decorators import action
@@ -11,6 +12,20 @@ class QuestionViewSet(viewsets.GenericViewSet):
     serializer_class = QuestionSerializer
     
     @swagger_auto_schema(tags=[SWAGGER_QUESTION_TAG])
-    @action(detail=False, methods=[GET])
-    def test_url(self, request):
-        return Response("Test URL")
+    @action(detail=False, methods=[POST])
+    def make_qustion(self, request):
+        # serializer = QuestionSerializer(data=request.data)
+        # serializer.is_valid(raise_exception=True)
+        # question_params = serializer.get_fields()
+        
+        category = request.data.get("category", "")
+        difficulty = request.data.get("difficulty", "")
+
+        data = {
+            "difficulty": difficulty,
+            "category": category
+        }
+        response = requests.post(MAKE_QUESTION_URL, json=data)
+        response_data = response.json()
+        print(response_data)
+        return Response("Done : Create Question")
